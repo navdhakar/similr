ffaa211d3b5a430ec3336e410a417a18b1cbe239
navdhakar <navdepdhakar1@gmail.com>
2020-10-06 18:26:47 +0530

    database handaling using monk

--- server/server.js
+++ server/server.js
@@ -3,10 +3,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const monk = require('monk');
+const db = monk('localhost:27016/test');
+
+const collection = db.get('docu');
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.json('connected to back_end server');
+app.get('/', async (req, res) => {
+  try {
+    collection.insert([{ a: 1 }, { a: 2 }, { a: 3 }]);
+    const item = await collection.find();
+    console.log(item);
+    res.json(item);
+  } catch (error) {}
 });
 app.listen(5000, () => {
   console.log('dynamic server up and running port:5000');
