2e8193b5840e0f9b2956f054702725c69a7c5216
navdhakar <navdepdhakar1@gmail.com>
2020-10-08 19:42:02 +0530

    basic login user verfication added

--- client/login.js
+++ client/login.js
@@ -2,6 +2,7 @@
 
 console.log('script2 loaded');
 const form = document.querySelector('form');
+const url = 'http://localhost:5000/login';
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formdata = new FormData(form);
@@ -9,5 +10,21 @@ form.addEventListener('submit', (event) => {
     email: formdata.get('email'),
     password: formdata.get('password'),
   };
-  console.log(userlogindata);
+  fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors',
+    body: JSON.stringify(userlogindata),
+    // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body data type must match "Content-Type" header
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 });
--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const db = monk('localhost:27016/test');
 // db.options = {
 //   safe: true,
 //   castIds: false,
-// };
-const userdata = db.get('userd');
+// };//code to disable autocasting o id's;
+const usercreds = db.get('userd');
 app.use(cors());
 app.use(express.json());
 app.get('/', async (req, res) => {});
@@ -19,8 +19,42 @@ app.listen(5000, () => {
   console.log('dynamic server up and running port:5000');
 });
 app.post('/', async (req, res) => {
-  await userdata.insert(req.body);
-  const dat = await userdata.find({ name: 'navdeep' }, 'email');
-  console.log(dat);
-  res.json(dat);
+  const new_user_creds = await usercreds.insert(req.body);
+  // const dat = await userdata.find();
+  // console.log(dat);
+  res.json('you are registered successfully');
 });
+// function verifyuser(pass){
+//   for (var i = 0; i < user_no; i++) {
+//     if (pass === users_records) {
+//       console.log('verified user logged in');
+//       res.json('you are a verified user');
+//     } else {
+//       continue;
+//     }
+// }
+app.get('/', async (req, res) => {
+  const users_records = await usercreds.find();
+  res.json(users_records);
+});
+app.post('/login', async (req, res) => {
+  const current_user = req.body;
+  const user_records = await usercreds.find({ email: current_user.email }, 'password');
+  // const some = current_user.password === users_records.docs.password;
+  // console.log(some);
+  if (current_user.password === user_records[0].password && user_records != undefined) {
+    res.json('you are a verified user');
+    console.log('verified user looged in');
+  } else {
+    res.json('you are an imposter');
+    console.log('unverified user');
+  }
+});
+
+// for (var i = 0; i < user_no; i++) {
+//   if (current_user.password === users_records) {
+//     console.log('verified user logged in');
+//     res.json('you are a verified user');
+//   } else {
+//     res.json('you are an imposter');
+//   }
