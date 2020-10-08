8608a07fd22f1d3fb32af16395c498894055774d
navdhakar <navdepdhakar1@gmail.com>
2020-10-08 13:31:49 +0530

    database,dynamic server and static server con test

--- client/signup.js
+++ client/signup.js
@@ -3,6 +3,7 @@ console.log('script loaded');
 const form = document.querySelector('form');
 var signup = document.getElementsByClassName('signup');
 var e = document.getElementById('occupation');
+const url = 'http://localhost:5000/';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -18,5 +19,21 @@ form.addEventListener('submit', (event) => {
     occupation: occu,
     password: password,
   };
-  console.log(userdata);
+  fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors',
+    body: JSON.stringify(userdata),
+    // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body data type must match "Content-Type" header
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 });
--- server/server.js
+++ server/server.js
@@ -4,19 +4,23 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const monk = require('monk');
-const db = monk('localhost:27016/test');
-
-const collection = db.get('docu');
 app.use(cors());
 app.use(express.json());
-app.get('/', async (req, res) => {
-  try {
-    collection.insert([{ a: 1 }, { a: 2 }, { a: 3 }]);
-    const item = await collection.find();
-    console.log(item);
-    res.json(item);
-  } catch (error) {}
-});
+const db = monk('localhost:27016/test');
+// db.options = {
+//   safe: true,
+//   castIds: false,
+// };
+const userdata = db.get('userd');
+app.use(cors());
+app.use(express.json());
+app.get('/', async (req, res) => {});
 app.listen(5000, () => {
   console.log('dynamic server up and running port:5000');
+});
+app.post('/', async (req, res) => {
+  await userdata.insert(req.body);
+  const dat = await userdata.find({ name: 'navdeep' }, 'email');
+  console.log(dat);
+  res.json(dat);
 });
